10cc9bd56fb65a1110c7151a8427d2898bb6e41c
Eric Mathews <eam34m@gmail.com>
2023-08-01 14:01:18 -0700

    fixed rewriting of used built-in str

--- arrays-and-hashing/encode-and-decode-strings/eads1a.py
+++ arrays-and-hashing/encode-and-decode-strings/eads1a.py
@@ -11,24 +11,24 @@ class Solution:
             string: encoded string containing strings from list
         """
         res = ''
-        for str in strs:
-            res += str(len(s)) + '#' + str
+        for s in strs:
+            res += str(len(s)) + '#' + s
         return res
 
-    def decode(self, str):
+    def decode(self, s):
         """Decodes the string into a list of strings
 
         Args:
-            str (string): encoded string to be decoded
+            s (string): encoded string to be decoded
         Returns:
             list: list of strings contained within the encoded string
         """
         res, i = [], 0
-        while i < len(str):
+        while i < len(s):
             j = i
             while j != '#':
                 j += 1
-            length = int(str[i:j])
-            res.append(str[j + 1: j + 1 + length])
+            length = int(s[i:j])
+            res.append(s[j + 1: j + 1 + length])
             i = j + 1 + length
         return res
