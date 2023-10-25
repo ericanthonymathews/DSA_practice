266220b6519928a2431f2ea676a04d1ca83c68cf
Eric Mathews <eam34m@gmail.com>
2023-10-25 14:09:37 -0700

    removed linting error

--- study-problems/find-nth-root/fnr1a.py
+++ study-problems/find-nth-root/fnr1a.py
@@ -1,5 +1,4 @@
 def root(x, n):
-
     pass  # your code goes here
     l, r = 0, x
     mid = (l + r) / 2.0
@@ -13,36 +12,6 @@ def root(x, n):
             return mid
         mid = (l + r) / 2.0
     return mid
-    '''
-  input: float, integer
-  output: integer
-  edgecases:
-  outliers:
-  y^n = x
-
-  input: x:7 n:3
-  l:0
-  r:6
-  m:3
-  m = (l + r / 2)
-  27
-  r = m
-  l:0
-  r:3
-  m:1.5
-  m = (l + r / 2)
-  1^3->3.375
-  l = m
-  l:1.5
-  r:3
-  m:2.25
-  m = (l + r / 2)
-  2.25^3->3.375
-  l = m
-  ...
-  y^n = x
-  if abs(y - root(x,n)) < 0.001
-  '''
 
 # binary search - log search
 # usual: while (l <= r) and (y - root(x,n))
