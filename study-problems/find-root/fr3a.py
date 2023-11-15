d85bba6c84d5eaa913c00c9f189394723525d6e7
Eric Mathews <eam34m@gmail.com>
2023-11-15 13:19:40 -0800

    solution using logarithms

---
+++ study-problems/find-root/fr3a.py
@@ -0,0 +1,12 @@
+import numpy as np
+# The result(D) is the nth root of x
+# K^(1/n)= D
+# logn(K^(1/n)) = logn(D)
+# (1/n)logn(K) = logn(D)
+# D = N^((1/n)logn(K))
+
+
+def root(k, n):
+    return pow(n, ((1.0 / n) *
+                   (np.log(k) /
+                    np.log(n))))
