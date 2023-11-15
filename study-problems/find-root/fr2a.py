e49d1a2a5fb4a9c8d6fafbcbe1a1853fa4633694
Eric Mathews <eam34m@gmail.com>
2023-11-15 13:15:57 -0800

    second attempt

---
+++ study-problems/find-root/fr2a.py
@@ -0,0 +1,15 @@
+def root(x, n):
+
+    pass  # your code goes here
+    l, r = 0, x
+    mid = (l + r) / 2.0
+    while mid - l >= 0.001:
+        y = mid ** n
+        if y > x:
+            r = mid
+        elif y < x:
+            l = mid
+        else:
+            return mid
+        mid = (l + r) / 2.0
+    return mid
